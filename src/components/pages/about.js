1f08184323233e1c5c85b9eeff6378ab8f65d3c9
Clovepi <cassandralpichardo@gmail.com>
2022-05-17 12:43:18 -0700

    finalizing

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -17,12 +17,20 @@ export default function() {
         I hope to get my bachelor's in Software Development at the end of this year (Dec 2022) before
         continuing on to get my master's in Computer Science next year. Currently, I'm a Mechatronics
         Technician for Amazon, but for years I was actually a Henna Tattoo Artist! I'm very artistic and
-        love to doodle or make music- I've been playing the violin for eleven years now, although sometimes
-        I defect to the piano or cello. Aside from that, I absolutely adore my 10-year-old little brother 
+        love to doodle or play music- I've been playing the violin for eleven years now, although sometimes
+        I think about defecting to the cello. Aside from that, I absolutely adore my 10-year-old little brother 
         and I enjoy playing video games or going on adventures with him. 
         <br />
         I'm a native Spanish speaker, although I love learning languages (not just coding ones!) and am 
-        conversational in Japanese. 
+        conversational in Japanese. Thanks for visiting!
+
+        {/* Hi, my name is Cassandra Pichardo. I'm 20 years old and a student at Western Governors University. 
+        I hope to get my bachelor's in Software Development at the end of this year (Dec 2022) before
+        continuing on to get my master's in Computer Science next year. Currently, I'm a Mechatronics
+        Technician for Amazon, but for years I was actually a Henna Tattoo Artist! I'm very artistic and
+        love to doodle or make music- I've been playing the violin for eleven years now, although sometimes
+        I defect to the piano or cello. Aside from that, I absolutely adore my 10-year-old little brother 
+        and I enjoy playing video games or going on adventures with him.  */}
       </div>
     </div>
   );
