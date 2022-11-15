ff67dd1e4a3fa93282e7e66122693878565a92a4
Clovepi <cassandralpichardo@gmail.com>
2022-11-15 01:29:20 -0700

    birthday update

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -14,12 +14,12 @@ export default function() {
       />
 
       <div className="right-column">
-        Hi, my name is Cassandra Pichardo. I'm 20 years old and a student at Western Governors University. 
-        I hope to get my bachelor's in Software Development at the end of this year (Dec 2022) before
-        continuing on to get my master's in Computer Science next year. Currently, I'm a Mechatronics
-        Technician for Amazon, but for years I was actually a Henna Tattoo Artist! I'm very artistic and
-        love to doodle or play music- I've been playing the violin for eleven years now, although sometimes
-        I think about defecting to the cello. Aside from that, I absolutely adore my 10-year-old little brother 
+        Hi, my name is Cassandra Pichardo. I'm 21 years old and a student at Western Governors University. 
+        I hope to get my bachelor's in Software Development by the end of next year (Fall 2023!) before
+        continuing on to get my master's in Computer Science. Currently, I'm an insurance adjuster for State
+        Farm, but for years I was actually a Henna Tattoo Artist! I'm very artistic and
+        love to draw or play music- I've been playing the violin for twelve years now, although sometimes
+        I think about defecting to the cello. Aside from that, I absolutely adore my 11-year-old little brother 
         and I enjoy playing video games or going on adventures with him. 
         <br />
         I'm a native Spanish speaker, although I love learning languages (not just coding ones!) and am 
